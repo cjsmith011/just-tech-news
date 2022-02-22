ba8e0b29063eaafa448a6526cb9d43cc2bc05880
Ronald Smith <ronaldsmith9090@comcast.net>
2022-02-21 20:37:29 -0500

    created routes for posts by users

--- models/index.js
+++ models/index.js
@@ -1,3 +1,15 @@
 const User = require('./User');
+const Post = require('./Post')
 
-module.exports = { User };
+
+//create associations, for this one: a user can have many posts and the fk helps associate them to each other
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+//create association, a post can only belong to one user
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+});
+
+
+module.exports = { User, Post };
